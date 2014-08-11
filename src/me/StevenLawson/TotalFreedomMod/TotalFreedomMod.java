11a2b897808a18126cc4081e6eeb41098ff71f53
Brandon <kellatech38@gmail.com>
2014-08-11 19:17:06 +0100

    Update TotalFreedomMod.java

--- src/me/StevenLawson/TotalFreedomMod/TotalFreedomMod.java
+++ src/me/StevenLawson/TotalFreedomMod/TotalFreedomMod.java
@@ -40,6 +40,8 @@ public class TotalFreedomMod extends JavaPlugin
     //
     public static final String MSG_NO_PERMS = ChatColor.YELLOW + "You do not have permission to use this command.";
     public static final String YOU_ARE_OP = ChatColor.YELLOW + "You are now op!";
+    public static final String YOU_ARE_ADMIN = ChatColor.YELLOW + "You are now a Super Admin!";
+    public static final String YOU_ARE_NOT_ADMIN = ChatColor.YELLOW + "You are no longer a Super Admin!";
     public static final String YOU_ARE_NOT_OP = ChatColor.YELLOW + "You are no longer op!";
     public static final String CAKE_LYRICS = "But there's no sense crying over every mistake. You just keep on trying till you run out of cake.";
     public static final String NOT_FROM_CONSOLE = "This command may not be used from the console.";
