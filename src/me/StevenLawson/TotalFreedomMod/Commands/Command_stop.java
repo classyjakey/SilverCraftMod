cccbb1e85bf235f76c4c0e27b29d98913ceb7669
aggelosQQ <jouflis1@hotmail.com>
2014-08-11 13:50:04 +0300

    Update Command_stop.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_stop.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_stop.java
@@ -6,7 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
 @CommandParameters(description = "Kicks everyone and stops the server.", usage = "/<command>")
 public class Command_stop extends TFM_Command
 {
