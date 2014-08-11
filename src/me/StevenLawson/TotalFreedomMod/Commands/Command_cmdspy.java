a2cf6c9e7ee59992b87aac601f04e70749fe485d
aggelosQQ <jouflis1@hotmail.com>
2014-08-11 13:29:48 +0300

    Update Command_cmdspy.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_cmdspy.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_cmdspy.java
@@ -6,16 +6,15 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Spy on commands", usage = "/<command>", aliases = "commandspy")
+@CommandParameters(description = "Spy on commands", usage = "/<command>")
 public class Command_cmdspy extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
 
-        TFM_PlayerData playerdata = TFM_PlayerData.getPlayerData(sender_p);
-        playerdata.setCommandSpy(!playerdata.cmdspyEnabled());
-        playerMsg("CommandSpy " + (playerdata.cmdspyEnabled() ? "enabled." : "disabled."));
+        
+        playerMsg("Please use /commandspy on or /c on"));
 
         return true;
     }
