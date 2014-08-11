85246db94cdbadc8f059162974c27d8324f49db4
aggelosQQ <jouflis1@hotmail.com>
2014-08-11 13:36:01 +0300

    Update Command_desuperall.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_desuperall.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_desuperall.java
@@ -13,7 +13,7 @@ public class Command_desuperall extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.adminAction(sender.getName(), "De-supering all players on the server", true);
+        TFM_Util.adminAction(sender.getName(), " > De-supering all players on the server", true);
 
         for (Player player : server.getOnlinePlayers())
         {
