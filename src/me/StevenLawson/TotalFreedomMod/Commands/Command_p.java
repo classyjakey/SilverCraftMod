ed4fe9676d35515226bc5965ec68f79f77b7ce53
GreatRaider <radionknife@gmail.com>
2014-08-13 20:01:46 +0100

    Update Command_p.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_p.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_p.java
@@ -1,9 +1,10 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
-import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -14,7 +15,7 @@ public class Command_p extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        if (args.length = 0)
+        if (args.length == 0)
         {
           return false;
         }
