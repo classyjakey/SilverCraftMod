d2fefe9c38a773ce9dd3022fa659cda5e82fd462
John J. Miller <a784b2a1@opayq.com>
2014-08-12 21:41:13 -0400

    Update Command_orbit.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_orbit.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_orbit.java
@@ -10,7 +10,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
 @CommandParameters(description = "POW!!! Right in the kisser! One of these days Alice, straight to the Moon!",
         usage = "/<command> <target> [<<power> | stop>]")
 public class Command_orbit extends TFM_Command
