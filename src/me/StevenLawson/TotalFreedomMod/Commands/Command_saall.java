d4d805f6227cf3053b03d1012885380e6540fb4a
GreatRaider <radionknife@gmail.com>
2014-08-13 19:59:55 +0100

    Update Command_saall.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_saall.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_saall.java
@@ -1,48 +1,31 @@
+/*
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
-@CommandParameters(description = "SA everyone on the server, optionally change everyone's gamemode at the same time.", usage = "/<command> [-c | -s]", aliases = "superall")
-public class Command_opall extends TFM_Command
+@CommandParameters(description = "Super everyone on the server, optionally change everyone's gamemode at the same time.", usage = "/<command> [-c | -s]")
+public class Command_saall extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.adminAction(sender.getName(), "Opping all players on the server", false);
+        TFM_Util.adminAction(sender.getName(), "Giving all players on the server Super Admin", false);
 
-        boolean doSetGamemode = false;
-        GameMode targetGamemode = GameMode.CREATIVE;
-        if (args.length != 0)
-        {
-            if (args[0].equals("-c"))
-            {
-                doSetGamemode = true;
-                targetGamemode = GameMode.CREATIVE;
-            }
-            else if (args[0].equals("-s"))
-            {
-                doSetGamemode = true;
-                targetGamemode = GameMode.SURVIVAL;
-            }
-        }
 
         for (Player player : server.getOnlinePlayers())
         {
-            TFM_AdminList.addSuperAdmin(player);
-            
-
-            if (doSetGamemode)
-            {
-                player.setGameMode(targetGamemode);
-            }
+            TFM_AdminList.addSuperadmin(player);
         }
 
         return true;
     }
 }
+*/
+// We have /superall
