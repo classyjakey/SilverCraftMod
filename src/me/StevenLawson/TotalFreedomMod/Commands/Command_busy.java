0d5f87f9903fecde82056a0059a3784b1249bc54
GreatRaider <radionknife@gmail.com>
2014-08-13 20:09:41 +0100

    Update Command_busy.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_busy.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_busy.java
@@ -1,9 +1,11 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerData;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 
@@ -18,40 +20,40 @@ public class Command_busy extends TFM_Command
         TFM_PlayerData playerdata = TFM_PlayerData.getPlayerData(sender_p);
         if (playerdata.isBusy)
         {
-                if (TFM_AdminList.isSeniorAdmin(player))
+                if (TFM_AdminList.isSeniorAdmin(sender_p))
                 {
-                     player.setPlayerListName(ChatColor.GRAY + player.getName());
-                     TFM_PlayerData.getPlayerData(player).setTag("&8[&dOff Duty Senior Admin&8]");
+                     sender_p.setPlayerListName(ChatColor.GRAY + sender_p.getName());
+                     TFM_PlayerData.getPlayerData(sender_p).setTag("&8[&dOff Duty Senior Admin&8]");
                 }
-                else if (TFM_AdminList.isTelnetAdmin(player, true))
+                else if (TFM_AdminList.isTelnetAdmin(sender_p, true))
                 {
-                      player.setPlayerListName(ChatColor.GRAY + player.getName());
-                      TFM_PlayerData.getPlayerData(player).setTag("&8[&2Off Duty Telnet Admin&8]");
+                      sender_p.setPlayerListName(ChatColor.GRAY + sender_p.getName());
+                      TFM_PlayerData.getPlayerData(sender_p).setTag("&8[&2Off Duty Telnet Admin&8]");
                 }
-                else if (TFM_AdminList.isSuperAdmin(player))
+                else if (TFM_AdminList.isSuperAdmin(sender_p))
                 {
-                     player.setPlayerListName(ChatColor.GRAY + player.getName());
-                     TFM_PlayerData.getPlayerData(player).setTag("&8[&B Off Duty Super Admin&8]");
+                     sender_p.setPlayerListName(ChatColor.GRAY + sender_p.getName());
+                     TFM_PlayerData.getPlayerData(sender_p).setTag("&8[&B Off Duty Super Admin&8]");
                 }
                 TFM_Util.bcastMsg(sender.getName() + " Is now off duty");
                 playerdata.setBusy(false);
         }
         else
         {
-            if (TFM_AdminList.isSeniorAdmin(player))
+            if (TFM_AdminList.isSeniorAdmin(sender_p))
                 {
-                     player.setPlayerListName(ChatColor.GRAY + player.getName());
-                     TFM_PlayerData.getPlayerData(player).setTag("&8[&dSenior Admin&8]");
+                     sender_p.setPlayerListName(ChatColor.GRAY + sender_p.getName());
+                     TFM_PlayerData.getPlayerData(sender_p).setTag("&8[&dSenior Admin&8]");
                 }
-                else if (TFM_AdminList.isTelnetAdmin(player, true))
+                else if (TFM_AdminList.isTelnetAdmin(sender_p, true))
                 {
-                      player.setPlayerListName(ChatColor.GRAY + player.getName());
-                      TFM_PlayerData.getPlayerData(player).setTag("&8[&2Telnet Admin&8]");
+                      sender_p.setPlayerListName(ChatColor.GRAY + sender_p.getName());
+                      TFM_PlayerData.getPlayerData(sender_p).setTag("&8[&2Telnet Admin&8]");
                 }
-                else if (TFM_AdminList.isSuperAdmin(player))
+                else if (TFM_AdminList.isSuperAdmin(sender_p))
                 {
-                     player.setPlayerListName(ChatColor.GRAY + player.getName());
-                     TFM_PlayerData.getPlayerData(player).setTag("&8[&BSuper Admin&8]");
+                     sender_p.setPlayerListName(ChatColor.GRAY + sender_p.getName());
+                     TFM_PlayerData.getPlayerData(sender_p).setTag("&8[&BSuper Admin&8]");
                 }
                 TFM_Util.bcastMsg(sender.getName() + " Is now on duty");
                 playerdata.setBusy(true);
