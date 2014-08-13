35494bcb1671c4e9ab1d4e9f582d2b59107ee543
GreatRaider <radionknife@gmail.com>
2014-08-13 16:33:17 +0100

    Update Command_p.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_p.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_p.java
@@ -1,7 +1,7 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
-import me.StevenLawson.TotalFreedomMod.TFM_AdminList
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
 import org.bukkit.command.Command;
 import org.bukkit.command.ChatColor;
 import org.bukkit.command.CommandSender;
