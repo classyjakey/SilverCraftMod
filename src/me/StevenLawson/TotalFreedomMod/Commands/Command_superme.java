d524068c17bdc51e15b6fce9d5d36f7466d9c12f
aggelosQQ <jouflis1@hotmail.com>
2014-08-13 15:09:13 +0300

    Create Command_superme.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_superme.java
@@ -0,0 +1,22 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Supering yourself!", usage = "/<command>")
+public class Command_superme extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        TFM_Util.adminAction(sender.getName(), "Opping " + sender.getName(), false);
+        sender.addSuperadmin(player));
+        sender.sendMessage(You are now supered!);
+
+        return true;
+    }
+}
