78115ef58e0e8589c4b5f14bdd7d12c005fa80da
GreatRaider <radionknife@gmail.com>
2014-08-13 20:04:45 +0100

    Update Command_desuperall.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_desuperall.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_desuperall.java
@@ -1,7 +1,7 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
-import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,7 +13,7 @@ public class Command_desuperall extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.adminAction(sender.getName(), " > De-supering all players on the server", true);
+        TFM_Util.adminAction(sender.getName(), "De-supering all players on the server", true);
 
         for (Player player : server.getOnlinePlayers())
         {
