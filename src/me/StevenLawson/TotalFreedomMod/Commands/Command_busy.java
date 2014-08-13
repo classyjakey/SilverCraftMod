21af9f25b9ac0ae80c3d657c444a3b5d9f72f22b
GreatRaider <radionknife@gmail.com>
2014-08-13 14:15:21 +0100

    FEW FINALLY FINISHED MADE IT A TOGGLE o_O

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_busy.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_busy.java
@@ -15,22 +15,48 @@ public class Command_busy extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-      if (args.length == 1)
+        TFM_PlayerData playerdata = TFM_PlayerData.getPlayerData(sender_p);
+        if (playerdata.isBusy)
         {
-        if (args[0].equals("on"))
-        {
-        TFM_Util.adminAction(ChatColor.RED + sender.getName(), "I'm currently busy, please conatact another admin.", false);
-        return true;
+                if (TFM_AdminList.isSeniorAdmin(player))
+                {
+                     player.setPlayerListName(ChatColor.GRAY + player.getName());
+                     TFM_PlayerData.getPlayerData(player).setTag("&8[&dOff Duty Senior Admin&8]");
+                }
+                else if (TFM_AdminList.isTelnetAdmin(player, true))
+                {
+                      player.setPlayerListName(ChatColor.GRAY + player.getName());
+                      TFM_PlayerData.getPlayerData(player).setTag("&8[&2Off Duty Telnet Admin&8]");
+                }
+                else if (TFM_AdminList.isSuperAdmin(player))
+                {
+                     player.setPlayerListName(ChatColor.GRAY + player.getName());
+                     TFM_PlayerData.getPlayerData(player).setTag("&8[&B Off Duty Super Admin&8]");
+                }
+                TFM_Util.bcastMsg(sender.getName() + " Is now off duty");
+                playerdata.setBusy(false);
         }
-        else if (args[0].equals("off"))
+        else
         {
-      TFM_Util.adminAction(ChatColor.AQUA + sender.getName(), "I'm not busy anymore, you can conatact me now.", false);
-      return true;
-        }
+            if (TFM_AdminList.isSeniorAdmin(player))
+                {
+                     player.setPlayerListName(ChatColor.GRAY + player.getName());
+                     TFM_PlayerData.getPlayerData(player).setTag("&8[&dSenior Admin&8]");
+                }
+                else if (TFM_AdminList.isTelnetAdmin(player, true))
+                {
+                      player.setPlayerListName(ChatColor.GRAY + player.getName());
+                      TFM_PlayerData.getPlayerData(player).setTag("&8[&2Telnet Admin&8]");
+                }
+                else if (TFM_AdminList.isSuperAdmin(player))
+                {
+                     player.setPlayerListName(ChatColor.GRAY + player.getName());
+                     TFM_PlayerData.getPlayerData(player).setTag("&8[&BSuper Admin&8]");
+                }
+                TFM_Util.bcastMsg(sender.getName() + " Is now on duty");
+                playerdata.setBusy(true);
+            
         }
         return true;
     }
 }
-
-
-// Command officially made in freedomop.
