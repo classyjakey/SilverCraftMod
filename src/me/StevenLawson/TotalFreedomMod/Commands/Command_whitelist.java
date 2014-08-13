3e93e3b47cc8b7f68085e739cad4e98785cf7000
John J. Miller <a784b2a1@opayq.com>
2014-08-12 21:44:37 -0400

    Update Command_whitelist.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_whitelist.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_whitelist.java
@@ -56,7 +56,7 @@ public class Command_whitelist extends TFM_Command
         }
 
         // all commands past this line are superadmin-only
-        if (!(senderIsConsole || TFM_AdminList.isSuperAdmin(sender)))
+        if (!(senderIsConsole || TFM_AdminList.isSeniorAdmin(sender)))
         {
             sender.sendMessage(TotalFreedomMod.MSG_NO_PERMS);
             return true;
@@ -92,7 +92,7 @@ public class Command_whitelist extends TFM_Command
 
             if (player == null)
             {
-                player = me.StevenLawson.TotalFreedomMod.TFM_DepreciationAggregator.getOfflinePlayer(server, search_name);
+                player = server.getOfflinePlayer(search_name);
             }
 
             TFM_Util.adminAction(sender.getName(), "Adding " + player.getName() + " to the whitelist.", false);
@@ -114,7 +114,7 @@ public class Command_whitelist extends TFM_Command
 
             if (player == null)
             {
-                player = me.StevenLawson.TotalFreedomMod.TFM_DepreciationAggregator.getOfflinePlayer(server, search_name);
+                player = server.getOfflinePlayer(search_name);
             }
 
             if (player.isWhitelisted())
