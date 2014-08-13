94c84573e6a758efad53ee103245c02bdbb4c9be
GreatRaider <radionknife@gmail.com>
2014-08-13 19:43:22 +0100

    *Slow Clap*

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_desuper.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_desuper.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
 @CommandParameters(description = "Desuper a player.", usage = "/<command> <playername>")
-public class Command_deop extends TFM_Command
+public class Command_desuper extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
