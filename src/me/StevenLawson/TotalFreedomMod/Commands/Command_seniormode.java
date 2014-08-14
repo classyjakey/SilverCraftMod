2fc43f8b6a679cfd814ae360db1667c7696538be
PacksGamingHD <craftyswans@gmail.com>
2014-08-13 20:16:16 -0700

    Commit
    Senior not super

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_seniormode.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_seniormode.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH, blockHostConsole = true)
-@CommandParameters(description = "Close server to superadmins.", usage = "/<command> [on | off]")
+@CommandParameters(description = "Close server to senioradmins.", usage = "/<command> [on | off]")
 public class Command_seniormode extends TFM_Command
 {
     @Override
@@ -28,12 +28,12 @@ public class Command_seniormode extends TFM_Command
         else if (args[0].equalsIgnoreCase("on"))
         {
             TFM_ConfigEntry.SENIOR_ONLY_MODE.setBoolean(true);
-            TFM_Util.adminAction(sender.getName(), "Closing the server to non-superadmins.", true);
+            TFM_Util.adminAction(sender.getName(), "Closing the server to non-senioradmins.", true);
             for (Player player : server.getOnlinePlayers())
             {
                 if (!TFM_AdminList.isSeniorAdmin(player))
                 {
-                    player.kickPlayer("Server is now closed to superadmins.");
+                    player.kickPlayer("Server is now closed to senioradmins.");
                 }
             }
             return true;
