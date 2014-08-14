7d4bf15a27e27edf946debd1122d94b3a8952a09
GreatRaider <radionknife@gmail.com>
2014-08-14 12:18:06 +0100

    Command_vj.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_vj.java
@@ -0,0 +1,124 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_Ban;
+import me.StevenLawson.TotalFreedomMod.TFM_BanManager;
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerList;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TFM_UuidResolver;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
+@CommandParameters(description = "Vj's Special CMD", usage = "/<command> <playername>")
+public class Command_vj extends TFM_Command
+{
+    @Override
+    public boolean run(final CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length != 1)
+        {
+            return false;
+        }
+
+        final Player player = getPlayer(args[0]);
+
+        if (player == null)
+        {
+            sender.sendMessage(TotalFreedomMod.PLAYER_NOT_FOUND);
+            return true;
+        }
+        player.getWorld().createExplosion(player.getLocation(), 8F);
+        TFM_Util.adminAction(sender.getName(), "I AM REALLY DISIPOINTED IN YOU " + player.getName(), true);
+        player.getWorld().strikeLightning(player.getLocation());
+        player.getWorld().strikeLightning(player.getLocation());
+        TFM_Util.bcastMsg(sender.getName() + " Will face my purple wrath", ChatColor.RED);
+        player.getWorld().createExplosion(player.getLocation(), 8F);
+        TFM_Util.bcastMsg(sender.getName() + " Shall be sent to mars!", ChatColor.RED);
+        player.getWorld().strikeLightning(player.getLocation());
+        player.getWorld().strikeLightning(player.getLocation());
+
+        final String ip = player.getAddress().getAddress().getHostAddress().trim();
+
+        // remove from superadmin
+        if (TFM_AdminList.isSuperAdmin(player))
+        {
+            TFM_Util.adminAction(sender.getName(), "Removing " + player.getName() + " from the superadmin list.", true);
+            TFM_AdminList.removeSuperadmin(player);
+        }
+        player.getWorld().createExplosion(player.getLocation(), 8F);
+
+        // remove from whitelist
+        player.setWhitelisted(false);
+
+        // deop
+        player.setOp(false);
+
+        // ban IPs
+        for (String playerIp : TFM_PlayerList.getEntry(player).getIps())
+        {
+            TFM_BanManager.addIpBan(new TFM_Ban(playerIp, player.getName()));
+        }
+
+        // ban uuid
+        TFM_BanManager.addUuidBan(player);
+
+        // set gamemode to survival
+        player.setGameMode(GameMode.SURVIVAL);
+
+        // clear inventory
+        player.closeInventory();
+        player.getInventory().clear();
+
+        // ignite player
+        player.setFireTicks(10000);
+
+        // generate explosion
+        player.getWorld().createExplosion(player.getLocation(), 4F);
+
+        // Shoot the player in the sky
+        player.setVelocity(player.getVelocity().clone().add(new Vector(0, 20, 0)));
+        
+        // Begin messages again
+        TFM_Util.adminAction(sender.getName(), "Is Vj'ing " + player.getName(), true);
+
+        new BukkitRunnable()
+        {
+            @Override
+            public void run()
+            {
+                // strike lightning
+                player.getWorld().strikeLightning(player.getLocation());
+
+                // kill (if not done already)
+                player.setHealth(0.0);
+            }
+        }.runTaskLater(plugin, 2L * 20L);
+        
+        TFM_Util.adminAction(sender.getName(), "Is Slamming the vj hammer over " + player.getName(), true);
+
+        new BukkitRunnable()
+        {
+            @Override
+            public void run()
+            {
+                // message
+                TFM_Util.adminAction(sender.getName(), "Banning " + player.getName() + ", IP: " + ip, true);
+
+                // generate explosion
+                player.getWorld().createExplosion(player.getLocation(), 4F);
+
+                // kick player
+                player.kickPlayer(ChatColor.DARK_PURPLE + "The Lord of the VJHammer has spoken!");
+            }
+        }.runTaskLater(plugin, 3L * 20L);
+
+        return true;
+    }
+}
