54ed3e5299fd701ade211bdcde7f4b71a6053666
GreatRaider <radionknife@gmail.com>
2014-08-14 09:41:56 +0100

    Update Command_stop.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_stop.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_stop.java
@@ -1,6 +1,7 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import net.minecraft.util.org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -13,15 +14,25 @@ public class Command_stop extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.bcastMsg("Server is going offline!", ChatColor.LIGHT_PURPLE);
-
-        for (Player player : server.getOnlinePlayers())
+        if (args.length > 0)
         {
-            player.kickPlayer("Server is going offline, come back in about 20 seconds.");
+                 new BukkitRunnable()
+                 {
+                 @Override
+                 public void run()
+                 {
+                   // Announce
+                   TFM_Util.bcastMsg(sender.getName() + " is closing the server..", ChatColor.LIGHT_PURPLE);
+                 }
+                     }.runTaskLater(plugin, 3L * 20L);
+                     
+            for (Player player : server.getOnlinePlayers())
+            {
+                player.kickPlayer("Server is going offline" StringUtils.join(args, " "));
+            }
+
+            server.shutdown();
         }
-
-        server.shutdown();
-
         return true;
     }
 }
